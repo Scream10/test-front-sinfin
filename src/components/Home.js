0656601fc21493615a2887a602f1fe85903ee512
Nicolas Diot <nicolasdiot10@gmail.com>
2021-05-13 17:52:00 +0200

    add load more feature

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,71 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { slice, concat } from "lodash";
 
-class Home extends Component {
-  state = {
-    loading: true,
-    post: []
-  }
+const LENGTH = 100;
+const DATA = [...Array(LENGTH).keys()];
+const LIMIT = 6;
 
-  componentDidMount(){
+function Home() {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const [showMore, setShowMore] = useState(true);
+  const [posts, setPosts] = useState(slice(DATA, 0, LIMIT));
+  const [index, setIndex] = useState(LIMIT);
+
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response) => {
-      return response.json()
-    })
-    .then((result) => {
-      this.setState({post: result, loading: false})
-    })
-  }
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setLoading(true);
+          setPosts(result.slice(0, LIMIT));
+        },
+        (error) => {
+          setLoading(true);
+          setError(error);
+        }
+      )
+  }, [])
 
-  render() {
+  const loadMore = () => {
+    const newIndex = index + LIMIT;
+    const newShowMore = newIndex < LENGTH - 1;
+    const newPosts = concat(posts, slice(DATA, index, newIndex));
+    setIndex(newIndex);
+    setPosts(newPosts);
+    setShowMore(newShowMore);
+  };
+
+  if (error) {
+    return <div className="text-center my-3">Erreur : {error.message}</div>;
+  } else if (!loading) {
+    return <div className="text-center my-3">Chargement...</div>
+  } else {
     return (
       <div className="container">
         <h1>Nos dernières publications :</h1>
-        <hr/>
-        {this.state.loading ? <div>Chargement...</div> : 
-          <ul>
-            {this.state.post.slice(0, 6).map(post => (
-              <li key={post.id}>
-                <div className="card card-body mb-3">
-                  <h4>{post.title}</h4>
-                  <p>{post.body}</p>
+        <hr />
+        <div className="row">
+          {posts.map((post) => (
+            <div key={post.id} className="col-lg-4 col-sm-6 mb-4">
+              <div className="card h-100">
+                <img src="https://via.placeholder.com/350x150" alt="350x150" className="card-img-top" />
+                <div className="card-body">
+                  <a href="/">
+                    <h4 className="card-title">{post.title}</h4>
+                  </a>
+                  <p className="card-text mb-0">{post.body}</p>
                 </div>
-              </li>
-            ))}
-          </ul>
-        }
-        <button className="btn btn-primary">Charger plus</button>
+              </div>
+            </div>
+          ))}
+          <div className="d-flex justify-content-center mb-5">
+            {showMore && <button onClick={loadMore} className="btn btn-primary">Charger plus</button>}
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
